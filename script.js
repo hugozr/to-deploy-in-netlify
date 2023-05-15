ca2dd7772b119021144763b0af52785c188edc37
hzumaeta <hzumaeta@gmail.com>
2023-05-15 16:13:28 -0500

    js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Ejemplo de código JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+    // Acciones a realizar cuando el contenido se haya cargado
+    alert("¡El contenido se ha cargado!");
+  });
+  
